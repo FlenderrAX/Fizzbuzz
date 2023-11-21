a20b404ba213e55ae1aaebc3928f78ed5a2e966a
Amine <aminossky1@gmail.com>
2023-11-21 22:12:20 +0100

    Fix button image path and update answer options

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import tkinter as tk
 from tkinter import ttk
-from tkinter import messagebox
 from PIL import Image, ImageTk
 import random
 import json
@@ -13,12 +12,10 @@ class Question:
         self.wrong_answer = wrong_answer
 
 class Button(ttk.Button):
-    def __init__(self, container):
+    def __init__(self, container, image_path, command=None):
         super().__init__(container)
-
-        self.play_button = ImageTk.PhotoImage(Image.open("img/play_button.png"))
-        self.config(image=self.play_button)
-        self.place(x=0, y=0)
+        self.image = ImageTk.PhotoImage(Image.open(image_path))
+        self.config(image=self.image, command=command)
 
 class App(tk.Tk):
     def __init__(self):
@@ -32,16 +29,17 @@ class App(tk.Tk):
         self.background_label = tk.Label(self, image=self.background)
         self.background_label.place(x=0, y=0, relwidth=1, relheight=1)
 
-        self.button = Button(self)
-        self.button["command"] = self.play
+        self.button = Button(self, "img/play_button.png", self.play)
         self.button.place(x=533, y=250, anchor="center")
 
+        self.good_answers = 0
+        self.bad_answers = 0
+
     def play(self):
-        for widget in app.winfo_children():
+        for widget in self.winfo_children():
             widget.destroy()
-        self.play_background = ImageTk.PhotoImage(Image.open("img/background.png"))
-        self.play_background_label = tk.Label(self, image=self.background)
-        self.play_background_label.place(x=0, y=0, relwidth=1, relheight=1)
+        self.background_label = tk.Label(self, image=self.background)
+        self.background_label.place(x=0, y=0, relwidth=1, relheight=1)
         with open('save.json', 'r') as f:
             data = json.load(f)
 
@@ -51,18 +49,50 @@ class App(tk.Tk):
         question_label = tk.Label(self, text=random_question.question, font=("Arial", 13), bg="#ffffff")
         question_label.place(x=533, y=250, anchor="center")
         
-        correct_answer_button = ttk.Button(self, text=random_question.answer, command=self.correct_answer)
-        correct_answer_button.place(x=533, y=300, anchor="center")
+        self.correct_answer_button = ttk.Button(self, text=random_question.answer, command=lambda: self.answer(True))
+        self.correct_answer_button.place(x=533, y=300, anchor="center")
 
-        wrong_answer_button = ttk.Button(self, text=random_question.wrong_answer, command=self.wrong_answer)
-        wrong_answer_button.place(x=533, y=350, anchor="center")
+        self.wrong_answer_button = ttk.Button(self, text=random_question.wrong_answer, command=lambda: self.answer(False))
+        self.wrong_answer_button.place(x=533, y=350, anchor="center")
 
-    def correct_answer(self):
-        messagebox.showinfo("Correct", "You are correct!")
+        self.good_answers_count = tk.Label(self, text=f"Good answers: {self.good_answers}", font=("Arial", 13), bg="#ffffff")
+        self.good_answers_count.place(x=0, y=0)
 
-    def wrong_answer(self):
-        messagebox.showerror("Wrong", "You are wrong!")
+        self.result_label = tk.Label(self, font=("Arial", 13), bg="#ffffff")
+        self.result_label.place(x=533, y=400, anchor="center")
 
+        # Start the countdown
+        self.time_left = 10  # Change this to change the countdown time
+        self.countdown_label = tk.Label(self, text=f"Time left: {self.time_left}", font=("Arial", 13), bg="#ffffff")
+        self.countdown_label.place(x=533, y=450, anchor="center")
+        self.countdown()
+
+    def countdown(self):
+        if self.time_left > 0:
+            self.time_left -= 1
+            self.countdown_label.config(text=f"Time left: {self.time_left}")
+            self.after(1000, self.countdown)  # Call this method again after 1 second
+        else:
+            self.answer(False)  # Time's up, count as wrong answer
+
+    def answer(self, is_correct):
+        # Cancel the countdown
+        self.time_left = 0
+
+        # Disable the buttons
+        self.correct_answer_button.config(state='disabled')
+        self.wrong_answer_button.config(state='disabled')
+
+        if is_correct:
+            self.result_label.config(text="You are correct!", fg="green")
+            self.good_answers += 1
+            self.good_answers_count.config(text=f"Good answers: {self.good_answers}")
+        else:
+            self.result_label.config(text="You are wrong!", fg="red")
+            self.bad_answers += 1
+
+        # Wait for 2 seconds before loading the next question
+        self.after(2000, self.play)
 
 if __name__ == "__main__":
     app = App()
